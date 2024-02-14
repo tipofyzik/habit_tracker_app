1117d2ee648989d6daf78525fb248f1563252c96
tipofyzik <84290230+tipofyzik@users.noreply.github.com>
2024-02-14 21:24:31 +0400

    Delete HabitTrackingApp.py

--- HabitTrackingApp.py
+++
@@ -1,9 +0,0 @@
-from CLI import CommandLineInterface as CLI
-
-
-
-if __name__ == "__main__":
-    input_analyzer = CLI()
-    print("Welcome to the Habit tracker!")
-    while True:
-        input_analyzer.start_menu()
