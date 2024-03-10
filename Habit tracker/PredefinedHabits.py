13e43a3a5fb771b392c760322e02aa1b05f47d5f
tipofyzik <84290230+tipofyzik@users.noreply.github.com>
2024-03-10 14:18:16 +0400

    Add files via upload

--- Habit tracker/PredefinedHabits.py
+++ Habit tracker/PredefinedHabits.py
@@ -12,10 +12,10 @@ def predefine_habits() -> list[Habit]:
     Returns 5 predefined habits to demonstrate to the user what it could be.
 
     No parametes."""
-    habit1 = Habit("Brush my teeth", "Day", 21)
+    habit1 = Habit("Brush my teeth", "Day", 28)
     habit2 = Habit("Go to the gym", "Week", 4)
     habit3 = Habit("Go to my therapist", "Month", 6)
-    habit4 = Habit("Take medicines", "Hour", 6)
+    habit4 = Habit("Take medicines", "Hour", 8)
     habit5 = Habit("Plant trees for charity", "Year", 3)
     return [habit1, habit2, habit3, habit4, habit5] 
 
